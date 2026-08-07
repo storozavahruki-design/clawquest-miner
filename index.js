5442ec363c23c99460b3df49975f101c753fc92d
storozavahruki-design <storozavahruki@gmail.com>
2026-08-08 01:32:53 +0300

    Change server port to 10000 and bind to all interfaces

--- index.js
+++ index.js
@@ -245,9 +245,9 @@ app.get("/upload-tank", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 loadApiCodeStore().then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server is running on port ${PORT}`);
     });
 });
